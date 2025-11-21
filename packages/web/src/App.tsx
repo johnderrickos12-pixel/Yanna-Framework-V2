1afd955090d9fe44eda151d22df3d400378e8e2a
johnderrickos12-pixel <johnderrickos12@gmail.com>
2025-11-22 06:56:03 +0800

    feat(web): create root App component

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="bg-gray-900 text-white min-h-screen p-8">
+      <h1 className="text-4xl font-bold">C2 Control Panel</h1>
+    </div>
+  );
+}
+
+export default App;
